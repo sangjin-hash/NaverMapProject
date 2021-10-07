f6a2d6c27286521a96d0aae4666495541669b5ec
sangjin-hash <lsj9873@ajou.ac.kr>
2021-10-07 13:06:32 +0900

    Main Service Prototype Implemented

--- app/src/main/java/com/example/naverpractice/MainServiceActvity.java
+++ app/src/main/java/com/example/naverpractice/MainServiceActvity.java
@@ -3,11 +3,15 @@ package com.example.naverpractice;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PixelFormat;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -16,9 +20,17 @@ import android.view.SurfaceView;
 import android.view.Window;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainServiceActvity extends AppCompatActivity {
 
     private static final String TAG = "[MAIN SERVICE]";
+    private static final int SECTION1_SIZE = 18;
+
+    private HashMap<Integer, Boolean> testCase1 = new HashMap<Integer, Boolean>();
+
     ImageView parkingLot;
     SurfaceView surfaceView;
 
@@ -54,29 +66,66 @@ public class MainServiceActvity extends AppCompatActivity {
     class EmptyThread extends Thread{
 
         private SurfaceHolder holder;
+        private Paint paint;
 
         public EmptyThread(SurfaceHolder holder){
             this.holder = holder;
+            paint = new Paint();
+            paint.setStrokeWidth(5f);
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setColor(Color.RED);
         }
 
         @Override
         public void run() {
+            /* 칸마다 거리 와꾸재기
             Canvas canvas = holder.lockCanvas();
-            draw(canvas); // if문으로 묶어서 해야될듯 + 반복문
-            holder.unlockCanvasAndPost(canvas); // 최초 한번만 해야됌
+            canvas.drawLine(435,980,845,980,paint);
+            holder.unlockCanvasAndPost(canvas);*/
+
+            Canvas canvas = holder.lockCanvas();
+            init_testCase();
+            draw(canvas);
+            holder.unlockCanvasAndPost(canvas);
+
+            for(int i = 1; i < SECTION1_SIZE; i++){
+                try {
+                    sleep(1500);
+                    canvas = holder.lockCanvas();
+                    canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
+                    update_testCase(i);
+                    draw(canvas);
+                    holder.unlockCanvasAndPost(canvas);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
         }
 
         public void draw(Canvas canvas){
-            Paint paint = new Paint();
-            paint.setStrokeWidth(5f);
-            paint.setStyle(Paint.Style.FILL);
-            paint.setColor(Color.RED);
-
-            //가로 길이 : 20, 세로 길이 : 60
-            canvas.drawLine(470,1080,490,1080, paint);
-            canvas.drawLine(490,1080,490,1150, paint);
-            canvas.drawLine(490,1150,470,1150, paint);
-            canvas.drawLine(470,1150,470,1080, paint);
+            for(int i = 0; i<testCase1.size();i++){
+                if(testCase1.get(i) == false){
+                    canvas.drawRect(435+i*23, 1010, 458+i*23, 1100, paint);
+                    Log.d(TAG, i+"번째 UI");
+                }
+            }
         }
-   }
+    }
+
+    public void init_testCase(){
+        //차가 있다 -> true, 차가 없다 -> false, 제일 초기 화면에 대한 UI 구성
+        testCase1.put(0,false);
+
+        for(int i = 1 ; i < SECTION1_SIZE; i++){
+            testCase1.put(i,true);
+        }
+    }
+
+    public void update_testCase(int i){
+        for(int k = 0; k < SECTION1_SIZE; k++){
+            testCase1.put(k, true);
+            if(k == i) testCase1.put(k,false);
+        }
+    }
 }
