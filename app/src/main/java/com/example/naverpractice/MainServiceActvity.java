add3abb9ad1b32087c49b5b4964337361d3604a2
sangjin-hash <lsj9873@ajou.ac.kr>
2021-10-14 00:21:01 +0900

    Todo: Implement data structure for each section

--- app/src/main/java/com/example/naverpractice/MainServiceActvity.java
+++ app/src/main/java/com/example/naverpractice/MainServiceActvity.java
@@ -19,6 +19,7 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.Window;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -26,13 +27,20 @@ import java.util.Map;
 
 public class MainServiceActvity extends AppCompatActivity {
 
+
     private static final String TAG = "[MAIN SERVICE]";
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
+
     private static final int SECTION1_SIZE = 18;
+    private static final int SECTION2_SIZE = 24;
 
     private HashMap<Integer, Boolean> testCase1 = new HashMap<Integer, Boolean>();
 
     ImageView parkingLot;
     SurfaceView surfaceView;
+
+    private TextView latitude;
+    private TextView longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +55,7 @@ public class MainServiceActvity extends AppCompatActivity {
             @Override
             public void surfaceCreated(@NonNull SurfaceHolder holder) {
                 holder.setFormat(PixelFormat.TRANSPARENT);
-                EmptyThread empty = new EmptyThread(holder);
+                MainService empty = new MainService(holder);
                 empty.start();
             }
 
@@ -63,12 +71,12 @@ public class MainServiceActvity extends AppCompatActivity {
         });
     }
 
-    class EmptyThread extends Thread{
+    class MainService extends Thread {
 
         private SurfaceHolder holder;
         private Paint paint;
 
-        public EmptyThread(SurfaceHolder holder){
+        public MainService(SurfaceHolder holder) {
             this.holder = holder;
             paint = new Paint();
             paint.setStrokeWidth(5f);
@@ -78,19 +86,15 @@ public class MainServiceActvity extends AppCompatActivity {
 
         @Override
         public void run() {
-            /* 칸마다 거리 와꾸재기
-            Canvas canvas = holder.lockCanvas();
-            canvas.drawLine(435,980,845,980,paint);
-            holder.unlockCanvasAndPost(canvas);*/
-
             Canvas canvas = holder.lockCanvas();
             init_testCase();
             draw(canvas);
             holder.unlockCanvasAndPost(canvas);
 
-            for(int i = 1; i < SECTION1_SIZE; i++){
+            //SectionSize만 바꿔주면 됌
+            for (int i = 0; i < SECTION2_SIZE; i++) {
                 try {
-                    sleep(1500);
+                    sleep(500);
                     canvas = holder.lockCanvas();
                     canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
                     update_testCase(i);
@@ -100,32 +104,118 @@ public class MainServiceActvity extends AppCompatActivity {
                     e.printStackTrace();
                 }
             }
-
         }
 
-        public void draw(Canvas canvas){
-            for(int i = 0; i<testCase1.size();i++){
-                if(testCase1.get(i) == false){
-                    canvas.drawRect(435+i*23, 1010, 458+i*23, 1100, paint);
-                    Log.d(TAG, i+"번째 UI");
+        public void draw(Canvas canvas) {
+            for (int i = 0; i < testCase1.size(); i++) {
+
+                /*제일 아래 1구역
+                if (testCase1.get(i) == false) {
+                    canvas.drawRect(432 + i * 23, 1010, 455 + i * 23, 1100, paint);
+                    Log.d(TAG, i + "번째 UI");
+                }*/
+
+                //2구역
+                if (testCase1.get(i) == false) {
+                    canvas.drawRect(432 + i * 23, 840, 455 + i * 23, 930, paint);
+                    Log.d(TAG, i + "번째 UI");
                 }
+
+                /*3구역
+                if (testCase1.get(i) == false) {
+                    canvas.drawRect(432 + i * 23, 660, 455 + i * 23, 750, paint);
+                    Log.d(TAG, i + "번째 UI");
+                }*/
+
+                /*4구역
+                if (testCase1.get(i) == false) {
+                    canvas.drawRect(432 + i * 23, 495, 455 + i * 23, 585, paint);
+                    Log.d(TAG, i + "번째 UI");
+                }*/
+
+                /*5구역
+                if (testCase1.get(i) == false) {
+                    canvas.drawRect(432 + i * 23, 315, 455 + i * 23, 405, paint);
+                    Log.d(TAG, i + "번째 UI");
+                }*/
+
+
+                /*6구역
+                if (testCase1.get(i) == false) {
+                    canvas.drawRect(409 + i * 23, 150, 431 + i * 23, 240, paint);
+                    Log.d(TAG, i + "번째 UI");
+                }*/
+
+
+                /*7구역
+                if (testCase1.get(i) == false) {
+                    canvas.drawRect(17 + i * 23, 150, 40 + i * 23, 240, paint);
+                    Log.d(TAG, i + "번째 UI");
+                }*/
+
+
+                /*8구역
+                if (testCase1.get(i) == false) {
+                    canvas.drawRect(63 + i * 23, 315, 86 + i * 23, 405, paint);
+                    Log.d(TAG, i + "번째 UI");
+                }*/
+
+
+                /*9구역
+                if (testCase1.get(i) == false) {
+                    canvas.drawRect(63 + i * 23, 495, 86 + i * 23, 585, paint);
+                    Log.d(TAG, i + "번째 UI");
+                }*/
+
+                /*10구역
+                if (testCase1.get(i) == false) {
+                    canvas.drawRect(40 + i * 23, 660, 63 + i * 23, 750, paint);
+                    Log.d(TAG, i + "번째 UI");
+                }*/
+
+                /*11구역
+                if (testCase1.get(i) == false) {
+                    canvas.drawRect(224 + i * 23, 660, 247 + i * 23, 750, paint);
+                    Log.d(TAG, i + "번째 UI");
+                }*/
+
+                /*12구역
+                if (testCase1.get(i) == false) {
+                    canvas.drawRect(224 + i * 23, 840, 247 + i * 23, 930, paint);
+                    Log.d(TAG, i + "번째 UI");
+                }*/
+
+                /*13구역
+                if (testCase1.get(i) == false) {
+                    canvas.drawRect(942 + i * 23, 495, 965 + i * 23, 585, paint);
+                    Log.d(TAG, i + "번째 UI");
+                }*/
+
+                /*14구역
+                if (testCase1.get(i) == false) {
+                    canvas.drawRect(942 + i * 23, 660, 965 + i * 23, 750, paint);
+                    Log.d(TAG, i + "번째 UI");
+                }*/
             }
         }
     }
 
-    public void init_testCase(){
-        //차가 있다 -> true, 차가 없다 -> false, 제일 초기 화면에 대한 UI 구성
-        testCase1.put(0,false);
-
-        for(int i = 1 ; i < SECTION1_SIZE; i++){
-            testCase1.put(i,true);
+    public void init_testCase() {
+        //마찬가지로 섹션사이즈만 바꿔주면댐
+        for (int i = 0; i < SECTION2_SIZE; i++) {
+            testCase1.put(i, true);
         }
     }
 
-    public void update_testCase(int i){
-        for(int k = 0; k < SECTION1_SIZE; k++){
-            testCase1.put(k, true);
-            if(k == i) testCase1.put(k,false);
+    // TODO : Retrofit을 통해 지속적으로 통신하여 GET 하며 좌석(0번~179번)에 대한 빈 자리 유무를 계속 update 해야 한다.
+    public void update_testCase(int i) {
+        if (i == 0) {
+            testCase1.put(i, false);
+        } else {
+            for (int k = 0; k < SECTION2_SIZE; k++) {
+                testCase1.put(k, true);
+                if (k == i) testCase1.put(k, false);
+            }
         }
     }
 }
